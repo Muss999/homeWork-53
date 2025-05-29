990fe2a696a3a5321cffa254758523f74bca2b43
Musulmankul <mussangle1@gmail.com>
2025-05-29 13:26:34 +0600

    Added useState of tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import AddTaskForm from "./AddTaskForm/AddTaskForm";
 import "./App.css";
 import Task from "./Task/Task";
 
 function App() {
+    const [tasks, setTasks] = useState([
+        {
+            text: "Buy milk",
+            id: 97,
+        },
+        {
+            text: "Clean PC",
+            id: 89,
+        },
+    ]);
+
     return (
         <div className="container">
             <AddTaskForm />
